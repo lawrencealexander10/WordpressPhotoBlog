e0e50e8b654dfcd5d9586143f415ff321b0416da
Alex Tenn <connection.l10@gmail.com>
2016-07-17 20:04:18 -0400

    switch theme, added initial config

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -42,14 +42,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         'o5+u;#|~+^(`jdpWq^e,&C#]t/2|DG++E{hvXe(#&7d$y3AVM.bJsQ}rUxn)D6PW');
+define('SECURE_AUTH_KEY',  '8=y^6++M!088;Do-PLRKpfg[axHapSeGZD~SJENh|08O_@Q6R_hbz/s6:5W;I2@)');
+define('LOGGED_IN_KEY',    '8AdO?d,r$0cZB+I}z8Y29p&~:_Z|^D|c=D!eLPJV0KNYM2S~q{1O=yu-S0s3@_K~');
+define('NONCE_KEY',        'jf0`%tE[]~e2]hgYJA{:#S!P]96Q+y$G)oqi^271z$c9UF*8H-U& IETpkWnN|+f');
+define('AUTH_SALT',        'xzdXN7=j~z5g6{87Asds(4c7S[-Njb<!6& 1-]gJttHTB:4S,m}.lA+=E4N[+(|2');
+define('SECURE_AUTH_SALT', ';mrLmp8;R`[3vd rZLlEvk=ah:OOCEj}v}4Yt||z-d%.(Z83B?P)P_yzdLUu)2TU');
+define('LOGGED_IN_SALT',   ',4ol,Y|[h8~Fu]}:}|&Isw[h* |G!&cV5}~L:N4^LjQl<S|<;IfkG()7HNxy-C2[');
+define('NONCE_SALT',       'Vh1rr^+7il(glzxPLVM[.( .w?uk~?p;GdEs^h~QPVys[+QjKm.&6,vfi]$,G7:`');
 
 /**#@-*/
 
