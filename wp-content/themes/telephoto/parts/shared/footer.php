5c638bb807a4ba5c65ad9c7febdf588c38d6100d
Alex Tenn <connection.l10@gmail.com>
2016-07-23 13:52:55 -0400

    continued working on fonts and footer

--- wp-content/themes/telephoto/parts/shared/footer.php
+++ wp-content/themes/telephoto/parts/shared/footer.php
@@ -1,7 +1,7 @@
     </div>
 <div class="bottom-foot-wrap">
 	<footer id="site-footer">
-    <a href="<?php  echo(site_url()); ?>"> &copy; <?php echo date("Y"); ?>, Everlena Tenn  </a>
+    <a href="<?php  echo(site_url()); ?>"> &copy; <?php echo date("Y"); ?>, <?php echo the_author_meta( 'first_name', 1); ?> <?php echo the_author_meta( 'last_name', 1); ?> </a>
     <a href="<?php echo the_author_meta( 'insta', 1); ?>">Instagram</a>
     <a href="<?php echo the_author_meta( 'linkedin', 1); ?>">LinkedIn</a>
     <a href="mailto:<?php echo the_author_meta( 'user_email', 1); ?>">Email</a>
